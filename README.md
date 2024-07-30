4974dde801a54f412801f5dace0c5a5e7f5475d8
Till Simon <mail@tillsimon.com>
2024-07-30 18:44:19 +0200

    Go

---
+++ README.md
@@ -0,0 +1,19 @@
+# AI automation: Personalized outreach to new lead
+
+This AI agent [built with LangChain](https://www.langchain.com/langchain) takes the email address of a new lead (from a waitlist, newsletter signup or lead magnet), researches the company (based on the email domain if it's from a business), drafts a personalized initial email outreach and hands it to a human.
+
+## Use this agent in 1 minute
+
+Simply click the button below to deploy this AI agent to a Vercel serverless function.  
+Then trigger it from our web app or via our API and find the results in your gotoHuman inbox.
+
+[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fgotohuman%2Fgth-demo-lc-vercel-newlead&env=OPENAI_API_KEY&envDescription=This%20agent%20is%20using%20OpenAI's%20GPT-4o%20model&envLink=https%3A%2F%2Fplatform.openai.com%2Fapi-keys&project-name=ai-agent-lc-lead-researcher&repository-name=ai-agent-lc-lead-researcher&redirect-url=https%3A%2F%2Fapp.gotohuman.com%2Fadd-agent-from-template%2Fgth-demo-lc-vercel-newlead)
+
+- When asked for it, enter your [API key from OpenAI](https://platform.openai.com/api-keys) (OpenAI Sidemenu > __API keys__)
+
+- After your agent is deployed to your own serverless function, you are redirected to gotoHuman where you are asked to enter some config to tailor the agent behavior to your own needs.
+- Run your agent manually. Or [call our API](https://docs.gotohuman.com/run#trigger-events) when you get a new email from a lead to make the agent act more autonomously.
+- Watch your AI automation do its job [in your gotoHuman inbox](https://app.gotohuman.com)
+
+## Extend
+Homework: Make the last step of the workflow `sendEmail` send an email automatically.
---
+++ src/app/api/researchlead/route.js
@@ -0,0 +1,88 @@
+export const maxDuration = 60;
+export const dynamic = 'force-dynamic';
+
+import AiFlow from "gotohuman";
+import { ChatOpenAI } from "@langchain/openai";
+import { HumanMessage, SystemMessage } from "@langchain/core/messages";
+import { CheerioWebBaseLoader } from "@langchain/community/document_loaders/web/cheerio";
+
+export async function POST(request) {
+  const req = await request.json()
+  const model = new ChatOpenAI({ temperature: 0.75, model: "gpt-4o" });
+  const aiFlow = new AiFlow({
+    onTrigger: "newLead", agentId: "new-lead-researcher-lc", fetch: fetch.bind(globalThis)
+  })
+  aiFlow.step({id: "extractDomain", fn: async({flow, input}) => {
+    return extractDomain(input[0])
+  }})
+  aiFlow.step({id: "summarizeWebsite", fn: async({input}) => {
+    const scrapedWebsite = await readWebsiteContent(input);
+    const summarizedWebsite = await summarizeWebsite(model, scrapedWebsite);
+    return summarizedWebsite;
+  }})
+  aiFlow.step({id: "draftEmail", fn: async({input, config}) => {
+    return await draftEmail(model, input, config.senderName, config.senderCompanyDesc);    
+  }})
+  aiFlow.gotoHuman({id: "approveDraft"})
+  aiFlow.step({id: "sendEmail", fn: async({input}) => {
+    // send email if `approved`
+    await new Promise(resolve => setTimeout(resolve, 1000));
+  }})
+  const resp = await aiFlow.executeSteps(req);
+  return Response.json(resp)
+}
+
+function extractDomain(email) {
+  const domain = email.split('@').pop();
+  const regex = createDomainRegex();
+  return (!regex.test(domain)) ? `https://${domain}` : flow.SKIP_TO('draftEmail', 'no-domain')
+}
+
+const commonProviders = [
+  'gmail', 'yahoo', 'ymail', 'rocketmail',
+  'outlook', 'hotmail', 'live', 'msn',
+  'icloud', 'me', 'mac', 'aol',
+  'zoho', 'protonmail', 'mail', 'gmx'
+];
+
+function createDomainRegex() {
+  // Escape any special regex characters in the domain names
+  const escapedDomains = commonProviders.map(domain => domain.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'));
+  // Join the domains with the alternation operator (|)
+  const pattern = `(^|\\.)(${escapedDomains.join('|')})(\\.|$)`;
+  return new RegExp(pattern);
+}
+
+async function readWebsiteContent(url) {
+  const loader = new CheerioWebBaseLoader(url);
+  
+  const docs = await loader.load();
+  return docs.length ? (docs[0]?.pageContent || "") : "";
+}
+
+async function summarizeWebsite(model, webContent) {
+  const messages = [
+    new SystemMessage("You are a helpful website content summarizer. You will be passed the content of a scraped company website. Please summarize it in 250-300 words focusing on what kind of company this is, the services they offer and how they operate."),
+    new HumanMessage(webContent),
+  ];
+  
+  const summaryCompletion = await model.invoke(messages);
+  return summaryCompletion.content;
+}
+
+async function draftEmail(model, websiteContent, senderName, senderCompanyDesc) {
+  const noDomain = (websiteContent === 'no-domain')
+  
+  const messages = [
+    new SystemMessage(`You are a helpful sales expert, great at writing enticing emails.
+    You will write an email for ${senderName} who wants to reach out to a new prospect who left their email address. ${senderName} workd for the following company:
+    ${senderCompanyDesc}
+    Write no more than 300 words.
+    ${!noDomain ? 'It must be tailored as much as possible to the prospect\'s company based on the website information we fetched. Don\'t mention that we got the information from the website.' : ''}`),
+    new HumanMessage((noDomain ? `No additional information found abour the prospect` : `#Company website summary:
+    ${websiteContent}`)),
+  ];
+  
+  const emailDrafterCompletion = await model.invoke(messages);
+  return emailDrafterCompletion.content;
+}
